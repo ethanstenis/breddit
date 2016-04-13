ae8569f73ff5b5b7a68353908415406dfaf20ccc
Ethan Luke Stenis <ethanstenis@gmail.com>
2016-04-12 20:37:29 -0500

    SubbredditModalView Created

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,18 +9,7 @@ use App\Http\Controllers\Controller;
 
 class UsersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return \App\User::all();
-    }
-
-
-    /**
+	/**
      * Display the specified resource.
      *
      * @param  int  $id
@@ -29,52 +18,7 @@ class UsersController extends Controller
     public function show($id)
     {
         return \App\User::with([
-            'posts', 
-            'comments',
-            'subbreddits',
             'subscribedSubbreddits'
-            ])->find($id);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $user = \App\User::find($id);
-        if ($user->user_id = \Auth::user()->id) {
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = $request->password;
-        $user->save();
-
-        } else { 
-            return response ("Unauthorized", 403);
-        }
-
-        return $user;
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $user = \App\User::find($id);
-        if ($user->user_id = \Auth::user()->id) {
-        $user->delete();
-
-        } else { 
-            return response ("Unauthorized", 403);
-        }
-
-        return $user;
+        ])->find($id);
     }
 }
--- public/src/js/views/SubbredditsListView.js
+++ public/src/js/views/SubbredditsListView.js
@@ -1,43 +1,53 @@
 var Backbone = require('backbone');
-var _= require('underscore');
+var _ = require('underscore');
+
+var SubbredditModel = require('../models/SubbredditModel.js');
+var PostsListView = require('./PostsListView.js');
+var SubbredditModalView = require('./SubbredditModalView.js');
+
 
 var SubbredditsListView = Backbone.View.extend({
-  el: '<ul></ul>', // put parent container in 'el' variable, and rest in template
+	el: '<ul></ul>',
 
-  template: _.template('\
-      <% subbreddits.each(function(subbreddit) { %>\
-        <li><a data-id="<%= subbreddit.id %>" href="#"><%= subbreddit.get("title") %></a></li>\
-        <% }) %>\
-    '),
+	template: _.template('\
+		<% subbreddits.each(function(subbreddit) { %>\
+			<li><a id="subbreddit" data-id="<%= subbreddit.id %>" href="#"><%= subbreddit.get("name") %></a></li>\
+		<% }) %>\
+		<li><a href="#" id="add-subbreddit" data-reveal-id="modal">Add Subbreddit</a>\
+	'),
 
-    events: {
-      'click a': function(event) {
-        event.preventDefault();
-        var subbredditId = $(event.target).data('id');
-        var SubbredditModel = require('../models/SubbredditModel.js');
-        var subbreddit = new SubbredditModel({ id: subbredditId });
-        subbreddit.fetch({
-          success: function(){
-            var PostsListView = require('../views/PostsListView.js');
-            var postsListView = new PostsListView({
-              collection: subbreddit.get('posts')
-            });
-            $('#posts').html(postsListView.render().el);
-          }
-        });
-      }
-    },
+	events: {
+		'click #subbreddit': function(event) {
+			event.preventDefault();
+			var subbredditId = $(event.target).data('id');
+			var subbreddit = new SubbredditModel({id: subbredditId});
+			subbreddit.fetch({
+				success: function() {
+					var postsListView = new PostsListView({
+						collection: subbreddit.get('posts')
+					});
+					$('#posts').html(postsListView.render().el);
+				}
+			});
+		},
 
-  initialize: function() {
-    this.listenTo(this.collection, 'update', this.render);
-  },
+		'click #add-subbreddit': function(event) {
+			var subbredditModalView = new SubbredditModalView({
+				collection: this.collection
+			});
+			$('#modal').html(subbredditModalView.el);
+			subbredditModalView.render();
+		}
+	},
 
-  render: function() {
-    $(this.el).html(this.template({
-      subbreddits: this.collection
-    }));
-    return this;
-  }
+	initialize: function() {
+		this.listenTo(this.collection, 'update', this.render);
+	},
+
+	render: function() {
+		this.$el.html(this.template({ subbreddits: this.collection }));
+		return this;
+	}
 });
 
 module.exports = SubbredditsListView;
